3527f3a146d375c6b103ab9187b323617989bd29
Glen Griffiths <glengriffiths@Glens-MacBook-Pro.local>
2021-03-05 20:52:51 +0000

    Code Cleanup

--- BatchData.py
+++ BatchData.py
@@ -1,30 +1,20 @@
-#Task 1 and Task 2
-
-#importing necessary libraries to run code
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 from pyspark.sql.types import StructType
 
 #Setting up PySpark
 spark = SparkSession.builder \
-          .appName('Assignment1') \
+          .appName('Batch Analytics') \
           .config('config.option', 'value') \
           .getOrCreate()
 
-#Task 2a
-#Loading in COVID-19 CSV file
 covidData = spark.read.csv(path='covid19.csv', header=True, inferSchema=True)
 
-#Task 2b
 #Outputting data in CSV file
 covidData.show()
 
 #Printing off the schema for the CSV file
 covidData.printSchema()
-
-#Task 2c
-#Showing data before filtering
-covidData.show()
 
 #Showing data after filering
 filteredData = covidData.filter(covidData['continent'].isNotNull() & covidData['location'].isNotNull() \
@@ -32,13 +22,11 @@ filteredData = covidData.filter(covidData['continent'].isNotNull() & covidData['
  & covidData['total_deaths'].isNotNull() & covidData['new_deaths'].isNotNull())
 filteredData.show()
 
-#Task 3
 #Grouping by the location column, colleting identical data into groups
 #Reducing duplicates and showing max number deaths in that location
 filteredData.groupBy(['location']) \
 .agg(f.max('total_deaths').alias('total_deaths')).show()
 
-#Task 4
 #Grouping by location, showing min number of cases in that location
 filteredData.groupBy(['location']) \
 .agg(f.min('total_cases').alias('min_cases')).show()
